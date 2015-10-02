721bb5a5e3a194789e91fd149e310239b8ec2ca5
Valentin Emelyanov <valentin.html@gmail.com>
2015-10-02 18:44:35 +0300

    init repository

---
+++ portfolio/barbershop-borodinski/js/script.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+	var link = document.querySelector('.account-menu-enter');
+	var popup = document.querySelector('.form-popup');
+	var close = document.querySelector('.js-form-popup_close');
+	var form = popup
+	var login = form.querySelector('.login-field');
+	var password = form.querySelector('.password-field');
+	var storage = localStorage.getItem('login');
+
+	link.addEventListener('click', function(event) {
+		event.preventDefault();
+		popup.classList.add('js-form-popup_show');
+		if (storage) {
+			login.value = storage;
+			password.focus();
+		} else {
+			login.focus();
+		}
+	}, false);
+
+	form.addEventListener('submit', function(event){
+		event.preventDefault();
+		if (!login.value || !password.value) {
+			event.preventDefault();
+			console.log('Для входа в личный кабинет ввести логин и пароль');
+		}
+	}, false);
+
+	close.addEventListener('click', function(event) {
+		event.preventDefault();
+		popup.classList.remove('js-form-popup_show');
+	}, false);
+
+	window.addEventListener('keydown', function(event) {
+		if (event.keyCode == 27 && popup.classList.contains('js-form-popup_show')) {
+			popup.classList.remove('js-form-popup_show');
+		}
+	}, false);
+
+}
---
+++ portfolio/nerds/js/script.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+	var link = document.querySelector('.feed-back-link');
+	var popup = document.querySelector('.feed-back-form');
+	var close = document.querySelector('.js-feed-back-form_close');
+	var form = popup
+	var name = form.querySelector('.name-field');
+	var email = form.querySelector('.email-field');
+	var storage = localStorage.getItem('name');
+
+	link.addEventListener('click', function(event) {
+		event.preventDefault();
+		popup.classList.add('js-feed-back-form_show');
+		if (storage) {
+			name.value = storage;
+			email.focus();
+		} else {
+			name.focus();
+		}
+	}, false);
+
+	form.addEventListener('submit', function(event){
+		event.preventDefault();
+		if (!name.value || !email.value) {
+			event.preventDefault();
+			console.log('Для отправки необходимо заполнить все поля формы');
+		}
+	}, false);
+
+	close.addEventListener('click', function(event) {
+		event.preventDefault();
+		popup.classList.remove('js-feed-back-form_show');
+	}, false);
+
+	window.addEventListener('keydown', function(event) {
+		if (event.keyCode == 27 && popup.classList.contains('js-feed-back-form_show')) {
+			popup.classList.remove('js-feed-back-form_show');
+		}
+	}, false);
+
+	$(document).ready(function(){
+		$('a[href*=#]').bind("click", function(e){
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 700);
+			e.preventDefault();
+		});
+		return false;
+	});
+
+}
